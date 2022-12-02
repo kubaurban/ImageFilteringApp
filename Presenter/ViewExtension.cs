41219e47887392cba18f85b5ee5e417592396119
Jakub Urbański <kuba.urban01@gmail.com>
2022-12-02 21:50:26 +0100

    implement image modifier method

---
+++ Presenter/ViewExtension.cs
@@ -0,0 +1,20 @@
+﻿using Model;
+using Presenter.Brushes;
+using Presenter.Filters;
+using View;
+
+namespace Presenter
+{
+    internal static class ViewExtension
+    {
+        public static void ModifyImage(this IView view, RawImage image, IBrush brush, IFilter filter)
+        {
+            view.LockDrawArea();
+            foreach (var pixel in brush.GetBrushPixels(image))
+            {
+                view.SetPixel(pixel.X, pixel.Y, filter.Filter(pixel.Color));
+            }
+            view.UnlockDrawArea();
+        }
+    }
+}
