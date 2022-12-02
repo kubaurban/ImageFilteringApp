d463ca2062b0ede59441088385d331c0d65ee41d
Jakub Urbański <kuba.urban01@gmail.com>
2022-12-02 20:53:27 +0100

    add image load functionality

--- Presenter/AppManager.cs
+++ Presenter/AppManager.cs
@@ -13,6 +13,37 @@ namespace Presenter
         public AppManager(IView view)
         {
             View = view;
+
+            View.LoadedFilenameChanged += HandleLoadedFilenameChanged;
+        }
+
+        private void HandleLoadedFilenameChanged(object? sender, string filename)
+        {
+            View.ClearArea();
+            LoadImage(filename);
+
+            DrawLoadedImage();
+            View.RefreshArea();
+        }
+
+        private void LoadImage(string path)
+        {
+            var bitmap = new Bitmap(path);
+            var scale = Math.Max((double)bitmap.Width / View.CanvasSize.Width, bitmap.Height / (double)View.CanvasSize.Height);
+            LoadedImage = new LoadedImage(new Bitmap(bitmap, new((int)(bitmap.Width / scale), (int)(bitmap.Height / scale))));
+        }
+
+        private void DrawLoadedImage()
+        {
+            if (LoadedImage is not null)
+            {
+                View.LockDrawArea();
+                foreach (var pixel in LoadedImage.Pixels())
+                {
+                    View.SetPixel(pixel.X, pixel.Y, pixel.Color);
+                }
+                View.UnlockDrawArea(); 
+            }
         }
     }
 }
