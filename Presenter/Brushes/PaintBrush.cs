f45dc8add17ece1df38048ce3f39a2b436da2de3
Jakub Urbański <kuba.urban01@gmail.com>
2022-12-02 22:15:14 +0100

    implement PaintBrush class

--- Presenter/Brushes/PaintBrush.cs
+++ Presenter/Brushes/PaintBrush.cs
@@ -4,9 +4,30 @@ namespace Presenter.Brushes
 {
     internal class PaintBrush : IBrush
     {
+        public Point? Center { get; set; }
+        public int Radius { get; set; }
+
+        public PaintBrush(int radius, Point? center = null)
+        {
+            Center = center;
+            Radius = radius;
+        }
+
         public IEnumerable<Pixel> GetBrushPixels(RawImage image)
         {
-            throw new NotImplementedException();
+            var center = Center!.Value;
+            for (int y = center.Y - Radius; y < center.Y + Radius + 1; y++)
+            {
+                var x_start = (int)Math.Round(Math.Sqrt(Math.Pow(Radius, 2) - Math.Pow(y - center.Y, 2)) + center.X);
+                var x_end = 2 * center.X - x_start;
+                for (int x = x_start; x < x_end + 1; x++)
+                {
+                    if (x < 0 || y < 0 || x > image.Bitmap.Width - 1 || y > image.Bitmap.Height - 1)
+                        continue;
+
+                    yield return image[x, y];
+                }
+            }
         }
     }
 }
