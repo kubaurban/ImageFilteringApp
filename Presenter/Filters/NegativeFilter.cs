f69e5d20af8690d07466051fdf2d4386232499cb
Jakub Urbański <kuba.urban01@gmail.com>
2022-12-02 21:52:37 +0100

    add negative filter class

---
+++ Presenter/Filters/NegativeFilter.cs
@@ -0,0 +1,7 @@
+﻿namespace Presenter.Filters
+{
+    internal class NegativeFilter : IFilter
+    {
+        public Color Filter(Color color) => Color.FromArgb(255 - color.R, 255 - color.G, 255 - color.B);
+    }
+}
