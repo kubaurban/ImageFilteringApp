20e2776b8f6f98ddc13cfac6d5c45004a8aa8fbb
Jakub Urbański <kuba.urban01@gmail.com>
2022-12-02 14:51:09 +0100

    rename solution

--- View/GUI.cs
+++ View/GUI.cs
@@ -1,10 +1,26 @@
+using System.Windows.Forms.DataVisualization.Charting;
+
 namespace View
 {
     public partial class GUI : Form
     {
+        private Chart BrezierChart;
+
         public GUI()
         {
             InitializeComponent();
+
+            BrezierChart = new Chart();
+            tableLayoutPanel1.Controls.Add(BrezierChart, 0, 3);
+
+            BrezierChart.Titles.Add("Brezier");
+            BrezierChart.Series.Clear();
+            var series = BrezierChart.Series.Add("Brezier");
+            //series.ChartType = SeriesChartType.Spline;
+            series.Points.AddXY(1, 20);
+            series.Points.AddXY(2, 30);
+            series.Points.AddXY(3, 20);
+            series.Points.AddXY(4, 5);
         }
     }
 }
