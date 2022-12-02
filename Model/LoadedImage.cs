12554228d62e89a769f3dfb90fd330fb46ac771c
Jakub Urbański <kuba.urban01@gmail.com>
2022-12-02 20:04:26 +0100

    add base models

---
+++ Model/LoadedImage.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace Model
+{
+    public class LoadedImage
+    {
+        public Bitmap Bitmap { get; }
+
+        public LoadedImage(Bitmap bitmap) => Bitmap = bitmap;
+
+        public Color this[int x, int y] => Bitmap.GetPixel(x, y);
+    }
+}
---
+++ Model/RawImage.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace Model
+{
+    public class RawImage : LoadedImage
+    {
+        public bool[,] Touched { get; }
+
+        public RawImage(Bitmap bitmap) : base(bitmap) => Touched = new bool[Bitmap.Width, Bitmap.Height];
+
+        public RawImage(LoadedImage image) : base(image.Bitmap) => Touched = new bool[Bitmap.Width, Bitmap.Height];
+    }
+}
