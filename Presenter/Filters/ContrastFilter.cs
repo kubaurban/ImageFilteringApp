bb8caec6c02838bba0eceee39c09325bc3e9f731
Jakub Urbański <kuba.urban01@gmail.com>
2022-12-03 11:06:06 +0100

    implement contrast filter

--- Presenter/Filters/ContrastFilter.cs
+++ Presenter/Filters/ContrastFilter.cs
@@ -1,10 +1,14 @@
-﻿namespace Presenter.Filters
+﻿using Presenter.Extensions;
+
+namespace Presenter.Filters
 {
     internal class ContrastFilter : IFilter
     {
-        public Color Filter(Color color)
-        {
-            throw new NotImplementedException();
-        }
+        public int Contrast { get; set; }
+        private int ContrastFactor => 259 * (255 + Contrast) / (255 * (259 - Contrast));
+
+        public Color Filter(Color color) => Color.FromArgb(Filter(color.R), Filter(color.G), Filter(color.B)).Truncate();
+
+        private int Filter(int i) => ContrastFactor * (i - 128) + 128;
     }
 }
