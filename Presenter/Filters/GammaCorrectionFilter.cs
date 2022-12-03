1eadd4d318d703c0666911a5302f6a9c591bcd69
Jakub Urbański <kuba.urban01@gmail.com>
2022-12-03 11:14:41 +0100

    implement gamma correction filter

--- Presenter/Filters/GammaCorrectionFilter.cs
+++ Presenter/Filters/GammaCorrectionFilter.cs
@@ -2,9 +2,11 @@
 {
     internal class GammaCorrectionFilter : IFilter
     {
-        public Color Filter(Color color)
-        {
-            throw new NotImplementedException();
-        }
+        public double Gamma { get; set; }
+        private double GammaCorrection => 1 / Gamma;
+
+        public Color Filter(Color color) => Color.FromArgb(Filter(color.R), Filter(color.G), Filter(color.B));
+
+        private int Filter(int i) => (int)(Math.Pow(i / 255, GammaCorrection) * 255);
     }
 }
